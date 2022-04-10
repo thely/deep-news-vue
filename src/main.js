bb490e1b79821a716d73347e583d0a59488ad96b
Becky Brown <bardbarienne@gmail.com>
2022-04-10 17:31:35 -0400

    click-outside directive

--- src/main.js
+++ src/main.js
@@ -4,12 +4,30 @@ import VueSocketIOExt from 'vue-socket.io-extended';
 import io from 'socket.io-client';
 
 import store from "./store";
-import App from './App.vue'
+import App from "./App.vue";
+// import directive from "./utils/click-outside.js";
 
 Vue.config.productionTip = false;
 
 const socket = io('http://localhost:8081', { cors: ['*'] });
 Vue.use(VueSocketIOExt, socket, { store });
+
+// Vue.directive('click-outside', directive);
+
+Vue.directive('click-outside', {
+  bind: function (element, binding, vnode) {
+    element.clickOutsideEvent = function (event) {  //  check that click was outside the el and his children
+      if (!(element === event.target || element.contains(event.target))) { // and if it did, call method provided in attribute value
+        vnode.context[binding.expression](event);
+        // binding.value(); run the arg
+      }
+    };
+    document.body.addEventListener('click', element.clickOutsideEvent)
+  },
+  unbind: function (element) {
+    document.body.removeEventListener('click', element.clickOutsideEvent)
+  }
+});
 
 new Vue({
   store: store,
