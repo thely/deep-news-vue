d185f5503a95f521b8f6ebd8eac18d5c848720b7
Becky Brown <bardbarienne@gmail.com>
2022-04-11 00:35:42 -0400

    normalizing control values

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="controls">
-    <input type="range" min="0" max="5" step="0.1" v-model="freqValue" @input="freqMethod">
+    <input type="range" min="1" max="100" step="1" v-model="freqValue" @input="freqMethod">
     <label for="volume">Frequency</label>
 
-    <input type="range" min="0.001" max="0.005" step="0.001" v-model="modVal" @input="modMethod">
+    <input type="range" min="1" max="100" step="1" v-model="modVal" @input="modMethod">
     <label for="volume">Modulation</label>
 
     <button @click.prevent="censor">Censor something</button>
@@ -20,8 +20,8 @@ export default {
   },
   data() {
     return {
-      freqValue: 1.5,
-      modVal: 0.003,
+      freqValue: 20,
+      modVal: 20,
     }
   },
   methods: {
