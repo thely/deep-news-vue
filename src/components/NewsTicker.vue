3cc8f6769c862c9de54f3bccb9c83be5a51b82a6
Becky Brown <bardbarienne@gmail.com>
2022-04-15 08:22:24 -0400

    news ticker

--- src/components/NewsTicker.vue
+++ src/components/NewsTicker.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="ticker-wrap">
-    <div class="ticker">
+    <marquee-text :repeat="2" :duration="30">
       <div class="item">
         here is some text about the news
       </div>
@@ -13,7 +13,7 @@
       <div class="item">
         BREAKING: YOU'LL NEVER BELIEVE THIS
       </div>
-    </div>
+    </marquee-text>
   </div>
 </template>
 
@@ -28,32 +28,41 @@ export default {
   position: absolute;
   bottom: 0;
   width: 100%;
-  overflow: hidden;
+  overflow: clip;
   height: 4rem;
-  background-color: rgba(#000, 0.9); 
+  background-color: rgb(0, 4, 255); 
   padding-left: 100%;
   box-sizing: content-box;
 
-  .ticker {
-    display: inline-block;
-    height: 4rem;
-    line-height: 4rem;  
-    white-space: nowrap;
-    padding-right: 100%;
-    box-sizing: content-box;
+  .marquee-text-wrap {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
 
-    // -webkit-animation-iteration-count: infinite; 
-            animation-iteration-count: infinite;
-    // -webkit-animation-timing-function: linear;
-            animation-timing-function: linear;
-  //  -webkit-animation-name: ticker;
-           animation-name: ticker;
-    // -webkit-animation-duration: $duration;
-            animation-duration: 40s;
+  // .ticker {
+  //   display: inline-block;
+  //   height: 4rem;
+  //   line-height: 4rem;  
+  //   white-space: nowrap;
+  //   padding-right: 100%;
+  //   box-sizing: content-box;
+
+  //   // -webkit-animation-iteration-count: infinite; 
+  //           animation-iteration-count: infinite;
+  //   // -webkit-animation-timing-function: linear;
+  //           animation-timing-function: linear;
+  // //  -webkit-animation-name: ticker;
+  //          animation-name: ticker;
+  //   // -webkit-animation-duration: $duration;
+  //           animation-duration: 40s;
 
     .item {
-
       display: inline-block;
+      font: "Helvetica Neue", sans-serif;
+      text-transform: uppercase;
+      font-weight: bold;
+      font-style: italic;
 
       padding: 0 2rem;
       font-size: 2rem;
@@ -61,21 +70,21 @@ export default {
 
     }
 
-  }
+  // }
 
 }
 
-@keyframes ticker {
-  0% {
-    -webkit-transform: translate3d(0, 0, 0);
-    transform: translate3d(0, 0, 0);
-    visibility: visible;
-  }
+// @keyframes ticker {
+//   0% {
+//     -webkit-transform: translate3d(0, 0, 0);
+//     transform: translate3d(0, 0, 0);
+//     visibility: visible;
+//   }
 
-  100% {
-    -webkit-transform: translate3d(-100%, 0, 0);
-    transform: translate3d(-100%, 0, 0);
-  }
-}
+//   100% {
+//     -webkit-transform: translate3d(-100%, 0, 0);
+//     transform: translate3d(-100%, 0, 0);
+//   }
+// }
 
 </style>
